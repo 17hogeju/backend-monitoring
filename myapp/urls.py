3e315c821c00f6750e23cbef4c1e54d755c44af3
17hogeju <17hogeju@gmail.com>
2023-06-09 11:07:14 -0700

    comment code out

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,10 +5,10 @@ import sentry_sdk
 def trigger_error(request):
     division_by_zero = 1 / 0
 
-@sentry_sdk.configure_scope
-def trigger_error_2(request):
-    with sentry_sdk.start_span(op='test_operation', description='Test Operation'):
-        division_by_zero = 1 / 0
+# @sentry_sdk.configure_scope
+# def trigger_error_2(request):
+#     with sentry_sdk.start_span(op='test_operation', description='Test Operation'):
+#         division_by_zero = 1 / 0
 
 from .views import InventoreyView, HandledErrorView, UnHandledErrorView, CaptureMessageView
 
@@ -19,6 +19,6 @@ urlpatterns = [
     path('unhandled', UnHandledErrorView.as_view()),
     path('message', CaptureMessageView.as_view()),
     path('sentry-debug/', trigger_error),
-    path('sentry-debug-2/', trigger_error_2),
+    # path('sentry-debug-2/', trigger_error_2),
 
 ]
